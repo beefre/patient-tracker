539d422c312384973382e331776c797c7e18cef6
beefre <omarvalentin@gmail.com>
2023-07-18 18:16:01 -0600

    Commit inicial

---
+++ patient-tracker/src/test/java/org/bedu/patienttracker/PatientTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.patienttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
